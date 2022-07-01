9495a7da84f6620a202dd238f73209e0609e2f46
Samir Touira <samir.touira@epitech.eu>
2022-07-01 17:51:22 +0200

    server side dockerized

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', 'localhost'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['firstAppKey']),
   },
