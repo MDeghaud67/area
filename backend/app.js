3b37ad19f1c16bac56710917bd237c2773dc4c66
Michel Deghaud <michel.deghaud@epitech.eu>
2022-07-06 16:51:06 +0200

    mongo

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+/*mongoose.connect('mongodb+srv://jimbob:<PASSWORD>@cluster0-pme76.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));*/
+
+module.exports = app;
+
